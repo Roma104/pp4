7b3f21f546fceeffa23dd38857a6ec0ff31f04df
Roma104 <92829743+Roma104@users.noreply.github.com>
2023-03-05 21:44:16 +0100

    Zadanie 1(ostatnie poprawki [już na pewno])

--- Grupa.py
+++ Grupa.py
@@ -1,33 +1,32 @@
 class Grupa:
 
     def __init__(self):
+        self.studenci = []
 
-        self.studenci= []
-
-    def zapiszStudenta(self,student):
+    def zapisz_studenta(self,student):
 
         if len(self.studenci) < 10:
 
             self.studenci.append(student)
-            print(f"{student} został dodany do kursu.")
+            return print(f"{student} został dodany do kursu.")
 
         else:
 
-            print(f"Przykro nam {student}. Kurs jest pełen. Proszę o wybranie innej grupy.")
+            return print(f"Przykro nam {student}. Kurs jest pełen. Proszę o wybranie innej grupy.")
 
 
 s = Grupa()
 
-s.zapiszStudenta("Discord")
-s.zapiszStudenta("Jonatan - student aktorstwa, który jest moim współlokatorem")
-s.zapiszStudenta("Lilith")
-s.zapiszStudenta("Kicia")
-s.zapiszStudenta("Pioter")
-s.zapiszStudenta("Gabrysia")
-s.zapiszStudenta("Ao Lie")
-s.zapiszStudenta("Wukong")
-s.zapiszStudenta("Macaque")
-s.zapiszStudenta("Mei")
-s.zapiszStudenta("Lucyfer")
+s.zapisz_studenta("Discord")
+s.zapisz_studenta("Jonatan - student aktorstwa, który jest moim współlokatorem")
+s.zapisz_studenta("Lilith")
+s.zapisz_studenta("Kicia")
+s.zapisz_studenta("Pioter")
+s.zapisz_studenta("Gabrysia")
+s.zapisz_studenta("Ao Lie")
+s.zapisz_studenta("Wukong")
+s.zapisz_studenta("Macaque")
+s.zapisz_studenta("Mei")
+s.zapisz_studenta("Lucyfer")
 
 #oczywiscie nie umiem wybrać normalnie losowych imion, no nie mogę no, to silniejsze ode mnie 
